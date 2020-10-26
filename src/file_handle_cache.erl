2a3ff524a57d93edd92b7c493c1e17ecd3f266d1
Luke Bakken <lbakken@pivotal.io>
2020-10-26 09:38:51 -0700

    Modify logging (cc @dumbbell)

--- src/file_handle_cache.erl
+++ src/file_handle_cache.erl
@@ -622,35 +622,46 @@ clear_process_read_cache() ->
     ].
 
 get_pdict() ->
-    rabbit_log:debug("~p pid: ~p get ENTIRE process dict", [?MODULE, self()]),
+    %% rabbit_log:debug("~p pid: ~p get ENTIRE process dict", [?MODULE, self()]),
     get().
 
+get_pdict(fhc_age_tree=Key) ->
+    get(Key);
 get_pdict(Key) ->
-    case get(Key) of
-        undefined ->
-            rabbit_log:debug("~p pid: ~p get Key: ~p returned UNDEFINED", [?MODULE, self(), Key]),
-            try
-                throw(kaboom)
-            catch _:_:Stack ->
-                rabbit_log:debug("~p pid: ~p call stack: ~p", [?MODULE, self(), Stack])
-            end,
-            undefined;
-        Value ->
-            rabbit_log:debug("~p pid: ~p get Key: ~p", [?MODULE, self(), Key]),
-            Value
-    end.
+    handle_get_pdict(Key, get(Key)).
 
+handle_get_pdict(Key, undefined) ->
+    rabbit_log:debug("~p pid: ~p get Key: ~p returned UNDEFINED", [?MODULE, self(), Key]),
+    try
+        throw(get_pdict_undefined)
+    catch _:_:Stack ->
+        rabbit_log:debug("~p pid: ~p call stack: ~p", [?MODULE, self(), Stack])
+    end,
+    undefined;
+handle_get_pdict(Key, Value) ->
+    rabbit_log:debug("~p pid: ~p get Key: ~p", [?MODULE, self(), Key]),
+    Value.
+
+put_pdict(fhc_age_tree=Key, Value) ->
+    put(Key, Value);
 put_pdict(Key, Value) ->
-    case Value of
-        undefined ->
-            rabbit_log:debug("~p pid: ~p put Key: ~p Value: UNDEFINED", [?MODULE, self(), Key]);
-        _ ->
-            rabbit_log:debug("~p pid: ~p put Key: ~p", [?MODULE, self(), Key])
+    rabbit_log:debug("~p pid: ~p put Key: ~p Value: ~p", [?MODULE, self(), Key, Value]),
+    try
+        throw(put_pdict_undefined)
+    catch _:_:Stack ->
+        rabbit_log:debug("~p pid: ~p put call stack: ~p", [?MODULE, self(), Stack])
     end,
     put(Key, Value).
 
+erase_pdict(fhc_age_tree=Key) ->
+    erase(Key);
 erase_pdict(Key) ->
     rabbit_log:debug("~p pid: ~p erase Key: ~p", [?MODULE, self(), Key]),
+    try
+        throw(erase_pdict)
+    catch _:_:Stack ->
+        rabbit_log:debug("~p pid: ~p erase call stack: ~p", [?MODULE, self(), Stack])
+    end,
     erase(Key).
 
 %%----------------------------------------------------------------------------
